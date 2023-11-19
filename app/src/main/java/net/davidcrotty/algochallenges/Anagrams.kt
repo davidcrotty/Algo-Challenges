b66ac1a57b1be7d6099309d55deacc9f4157b540
davidcrotty <david.crotty@playsportsnetwork.com>
2023-11-19 12:33:49 +0000

    check if word is anagram

--- app/src/main/java/net/davidcrotty/algochallenges/Anagrams.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/Anagrams.kt
@@ -2,4 +2,30 @@ package net.davidcrotty.algochallenges
 
 class Anagrams {
 
+    fun isAnagram(s: String, t: String): Boolean {
+
+        val comparitor1 = s.toCharArray()
+        val comparitor2 = t.toCharArray()
+
+        val comparitor1Map = mutableMapOf<Char, Int>()
+        val comparitor2Map = mutableMapOf<Char, Int>()
+
+        for (char in comparitor1) {
+            if (comparitor1Map.contains(char)) {
+                comparitor1Map[char + 1] = 1
+            } else {
+                comparitor1Map[char] = 1
+            }
+        }
+
+        for (char in comparitor2) {
+            if (comparitor2Map.contains(char)) {
+                comparitor2Map[char + 1] = 1
+            } else {
+                comparitor2Map[char] = 1
+            }
+        }
+
+        return comparitor1Map == comparitor2Map
+    }
 }
--- app/src/test/java/net/davidcrotty/algochallenges/AnagramTest.kt
+++ app/src/test/java/net/davidcrotty/algochallenges/AnagramTest.kt
@@ -1,11 +1,29 @@
 package net.davidcrotty.algochallenges
 
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
 
 class AnagramTest {
 
     @Test
-    fun isAnagram() {
+    fun testWithAnagram() {
         val sut = Anagrams()
+        val s = "anagram"
+        val t = "nagaram"
+
+        val result = sut.isAnagram(s, t)
+
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun testWithoutAnagram() {
+        val sut = Anagrams()
+        val s = "rat"
+        val t = "car"
+
+        val result = sut.isAnagram(s, t)
+
+        assertEquals(false, result)
     }
 }
