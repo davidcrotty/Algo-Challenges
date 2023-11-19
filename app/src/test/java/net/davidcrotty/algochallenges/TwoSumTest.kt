a6de178a687c4906fd854b20b273f5923e2f5d86
davidcrotty <david.crotty@playsportsnetwork.com>
2023-11-19 13:01:59 +0000

    passing scenarios

--- app/src/test/java/net/davidcrotty/algochallenges/TwoSumTest.kt
+++ app/src/test/java/net/davidcrotty/algochallenges/TwoSumTest.kt
@@ -16,4 +16,28 @@ class TwoSumTest {
         assertEquals(0, result[0])
         assertEquals(1, result[1])
     }
+
+    @Test
+    fun twoSum2() {
+        val sut = TwoSum()
+        val nums = intArrayOf(3,2,4)
+        val target = 6
+
+        val result = sut.twoSum(nums, target)
+
+        assertEquals(1, result[0])
+        assertEquals(2, result[1])
+    }
+
+    @Test
+    fun twoSum3() {
+        val sut = TwoSum()
+        val nums = intArrayOf(3, 3)
+        val target = 6
+
+        val result = sut.twoSum(nums, target)
+
+        assertEquals(0, result[0])
+        assertEquals(1, result[1])
+    }
 }
