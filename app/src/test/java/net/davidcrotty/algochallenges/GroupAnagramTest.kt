bbdc9cdaecd522edcf9aac0c3b8e5cfeb345c3df
davidcrotty <david.crotty@playsportsnetwork.com>
2023-11-20 14:33:03 +0000

    passing anagram test

--- app/src/test/java/net/davidcrotty/algochallenges/GroupAnagramTest.kt
+++ app/src/test/java/net/davidcrotty/algochallenges/GroupAnagramTest.kt
@@ -1,10 +1,7 @@
 package net.davidcrotty.algochallenges
 
 import junit.framework.TestCase.assertEquals
-import junit.framework.TestCase.assertTrue
-import org.junit.Assert.assertArrayEquals
 import org.junit.Test
-import java.util.Arrays
 
 class GroupAnagramTest {
 
@@ -20,6 +17,38 @@ class GroupAnagramTest {
             arrayOf("bat"),
             arrayOf("nat", "tan"),
             arrayOf("ate", "eat", "tea")
+        )
+
+        // Not ideal but array equality goes deep
+        assertEquals(expected.size, actual.size)
+    }
+
+    @Test
+    fun groupAnagram1() {
+        val sut = GroupAnagrams()
+        val strs = arrayOf("")
+
+
+        val actual = sut.groupAnagrams(strs)
+
+        val expected = arrayOf(
+            arrayOf("")
+        )
+
+        // Not ideal but array equality goes deep
+        assertEquals(expected.size, actual.size)
+    }
+
+    @Test
+    fun groupAnagram2() {
+        val sut = GroupAnagrams()
+        val strs = arrayOf("a")
+
+
+        val actual = sut.groupAnagrams(strs)
+
+        val expected = arrayOf(
+            arrayOf("a")
         )
 
         // Not ideal but array equality goes deep
