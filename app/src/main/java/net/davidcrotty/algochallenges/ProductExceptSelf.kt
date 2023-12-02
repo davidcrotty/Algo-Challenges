b1ab1c277e2ee55fb2d43923e065543af673bb75
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-02 11:21:36 +0000

    solve

--- app/src/main/java/net/davidcrotty/algochallenges/ProductExceptSelf.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/ProductExceptSelf.kt
@@ -3,6 +3,21 @@ package net.davidcrotty.algochallenges
 class ProductExceptSelf {
 
     fun productExceptSelf(nums: IntArray): IntArray {
-        return intArrayOf()
+
+        val end = nums.lastIndex
+        val products = IntArray(nums.size) { 1 }
+        var left = 1
+        var right = 1
+
+        // loop through each in array, produce array excluding that number
+        for(i in nums.indices) {
+          products[i] *= left
+          products[end - i] *= right
+
+          right *= nums[end - i]
+          left *= nums[i]
+        }
+
+        return products
     }
 }
