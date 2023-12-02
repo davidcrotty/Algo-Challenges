1d8d41a897a1525b42182e505355cabba68d2a46
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-02 12:18:13 +0000

    row and column solve

--- app/src/main/java/net/davidcrotty/algochallenges/SudokuSolver.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/SudokuSolver.kt
@@ -6,24 +6,25 @@ class SudokuSolver {
 
     fun solve(board: Array<Array<String>>): Boolean {
 
-        var isValid = true
+        for(col in board.indices) {
+            val colSet = mutableSetOf<Char>()
+            val rowSet = mutableSetOf<Char>()
+            val cube = mutableSetOf<Char>()
 
-        for (columnIndexed in board.withIndex()) {
-            val rowMap = mutableSetOf<Int>()
-            for (rowIndexed in columnIndexed.value.withIndex()) {
-                if (rowIndexed.value.matches(Regex("[0-9]"))) {
-                    rowMap.add(rowIndexed.value.toInt())
-                } else {
-                     rowMap += 10 + rowIndexed.index // ensure we dont clash
+            for (row in board.indices) {
+                val c = board[col][row]
+                val r = board[row][col]
+
+                if (c != "." && !colSet.add(c.toCharArray().first())) {
+                    return false
                 }
-            }
+                if (r != "." && !rowSet.add(r.toCharArray().first())) {
+                    return false
+                }
 
-            if (rowMap.size != 9) {
-                isValid = false
-                break
             }
         }
 
-        return isValid
+        return true
     }
 }
