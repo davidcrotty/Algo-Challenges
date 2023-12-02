8ca4641cadde6fe57aacf2a8a77c5e29ae65711c
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-02 11:51:13 +0000

    validate row

--- app/src/main/java/net/davidcrotty/algochallenges/SudokuSolver.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/SudokuSolver.kt
@@ -1,8 +1,29 @@
 package net.davidcrotty.algochallenges
 
+import androidx.core.text.isDigitsOnly
+
 class SudokuSolver {
 
     fun solve(board: Array<Array<String>>): Boolean {
-        return false
+
+        var isValid = true
+
+        for (columnIndexed in board.withIndex()) {
+            val rowMap = mutableSetOf<Int>()
+            for (rowIndexed in columnIndexed.value.withIndex()) {
+                if (rowIndexed.value.matches(Regex("[0-9]"))) {
+                    rowMap.add(rowIndexed.value.toInt())
+                } else {
+                     rowMap += 10 + rowIndexed.index // ensure we dont clash
+                }
+            }
+
+            if (rowMap.size != 9) {
+                isValid = false
+                break
+            }
+        }
+
+        return isValid
     }
 }
