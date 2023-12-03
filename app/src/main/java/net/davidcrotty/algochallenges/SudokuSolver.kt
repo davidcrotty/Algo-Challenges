d1853e87319c548b8183f84483f80f17691db92c
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-03 13:52:24 +0000

    solve for column

--- app/src/main/java/net/davidcrotty/algochallenges/SudokuSolver.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/SudokuSolver.kt
@@ -12,6 +12,14 @@ class SudokuSolver {
                  if(cell.value == '.') continue
                  if(!rowSet.add(cell.value)) return false
             }
+
+            // walk down
+            val columnSet = mutableSetOf<Char>()
+            for(index in board.indices) {
+                val cell = board[index][row.index]
+                if(cell == '.') continue
+                if(!columnSet.add(cell)) return false
+            }
         }
 
         return true
