92721657c26885266c3c7aefa8a6b9b05744ce08
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-03 13:39:53 +0000

    reset test

--- app/src/main/java/net/davidcrotty/algochallenges/SudokuSolver.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/SudokuSolver.kt
@@ -1,38 +1,10 @@
 package net.davidcrotty.algochallenges
 
-import androidx.core.text.isDigitsOnly
-
 class SudokuSolver {
 
     fun solve(board: Array<Array<String>>): Boolean {
 
-        for(col in board.indices) {
-            val colSet = mutableSetOf<Char>()
-            val rowSet = mutableSetOf<Char>()
-            val cube = mutableSetOf<Char>()
 
-            for (row in board.indices) {
-                val c = board[col][row]
-                val r = board[row][col]
-
-                if (c != "." && !colSet.add(c.toCharArray().first())) {
-                    return false
-                }
-                if (r != "." && !rowSet.add(r.toCharArray().first())) {
-                    return false
-                }
-
-                val colindex = 3*(col%3) + row%3
-                val rowindex = 3*(col/3) + row/3
-
-                val b = board[colindex][rowindex]
-
-                if (b != "." && !cube.add(b.toCharArray().first())) {
-                    return false
-                }
-            }
-        }
-
-        return true
+        return false
     }
 }
