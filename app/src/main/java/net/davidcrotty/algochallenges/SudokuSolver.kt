4e2d5b47aa149b210b8a4f3f829f5188c7fd783d
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-03 13:59:01 +0000

    own impl of cube solver

--- app/src/main/java/net/davidcrotty/algochallenges/SudokuSolver.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/SudokuSolver.kt
@@ -1,6 +1,20 @@
 package net.davidcrotty.algochallenges
 
+import android.graphics.Point
+
 class SudokuSolver {
+
+    val cubes = listOf(
+        Pair(0, 0),
+        Pair(0, 3),
+        Pair(0, 6),
+        Pair(3, 0),
+        Pair(3, 3),
+        Pair(3, 6),
+        Pair(6, 0),
+        Pair(6, 3),
+        Pair(6, 6)
+    )
 
     fun solve(board: Array<Array<Char>>): Boolean {
 
@@ -19,6 +33,17 @@ class SudokuSolver {
                 val cell = board[index][row.index]
                 if(cell == '.') continue
                 if(!columnSet.add(cell)) return false
+            }
+
+            // cube
+            val cubeSet = mutableSetOf<Char>()
+            val cube = cubes[row.index]
+            for(x in cube.first..cube.first + 2) {
+                for (y in cube.second..cube.second + 2) {
+                    val cell = board[x][y]
+                    if(cell == '.') continue
+                    if(!cubeSet.add(cell)) return false
+                }
             }
         }
 
