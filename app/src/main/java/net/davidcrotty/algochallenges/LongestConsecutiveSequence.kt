735ec09b36db3f63702c9d4da198badb341716c8
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-04 10:22:16 +0000

    solve O(n)

--- app/src/main/java/net/davidcrotty/algochallenges/LongestConsecutiveSequence.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/LongestConsecutiveSequence.kt
@@ -3,6 +3,39 @@ package net.davidcrotty.algochallenges
 class LongestConsecutiveSequence {
 
     fun longestConsecutive(nums: IntArray) : Int {
-        return 0
+
+        nums.sort()
+        val numList = nums.toList()
+        val iterator = numList.listIterator()
+
+        // loop using iterator, if next != current +1, store
+        var right: Int = -1
+        val longestList = mutableListOf<Int>()
+        var longest = 1
+
+        while(iterator.hasNext()) {
+            val current = iterator.next()
+            if (iterator.hasNext()) {
+                val right = iterator.next()
+                iterator.previous()
+
+                if (current + 1 == right) {
+                    // store sequence
+                    longest++
+                } else {
+                    longestList.add(longest)
+                    longest = 1
+                    // null sequence, move
+                }
+            }
+        }
+
+        if (longest != 1) {
+            longestList.add(longest)
+        }
+
+        longestList.sort()
+
+        return longestList.last()
     }
 }
--- app/src/test/java/net/davidcrotty/algochallenges/LongestConsecutiveSequenceTest.kt
+++ app/src/test/java/net/davidcrotty/algochallenges/LongestConsecutiveSequenceTest.kt
@@ -15,4 +15,15 @@ class LongestConsecutiveSequenceTest {
 
         assertEquals(expected, actual)
     }
+
+    @Test
+    fun longestConsecutiveSequence2() {
+        val expected = 9
+        val input = intArrayOf(0,3,7,2,5,8,4,6,0,1)
+
+        val sut = LongestConsecutiveSequence()
+        val actual = sut.longestConsecutive(input)
+
+        assertEquals(expected, actual)
+    }
 }
