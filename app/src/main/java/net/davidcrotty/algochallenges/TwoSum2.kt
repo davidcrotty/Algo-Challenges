68ed5b2dd78ca8d641f096462dff7230aa074c60
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-05 10:32:20 +0000

    two sum impl

--- app/src/main/java/net/davidcrotty/algochallenges/TwoSum2.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/TwoSum2.kt
@@ -3,6 +3,25 @@ package net.davidcrotty.algochallenges
 class TwoSum2 {
 
     fun twoSum2(input: IntArray, target: Int): IntArray {
+
+        val result = intArrayOf(0, 0)
+
+        for (i in 0 until (input.size - 1)) {
+            val left = input[i]
+
+            val list = input.toMutableList()
+            list.removeAt(i)
+            val iterator = list.iterator()
+            while(iterator.hasNext()) {
+                val next = iterator.next()
+                if (left + next == target) {
+                    result[0] = left
+                    result[1] = next
+                    return result
+                }
+            }
+        }
+
         return intArrayOf()
     }
 }
