030b20d57d2f2d30d09f36c743322aa6e7a8edfc
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-05 09:38:55 +0000

    validate palindrome

--- app/src/main/java/net/davidcrotty/algochallenges/ValidPalindrome.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/ValidPalindrome.kt
@@ -3,6 +3,10 @@ package net.davidcrotty.algochallenges
 class ValidPalindrome {
 
     fun isPalindrome(s: String): Boolean {
-        return false
+        val cleaned = Regex("[0-9!@#\$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/? ]").replace(s, "").lowercase()
+
+        val reverse = cleaned.reversed()
+
+        return cleaned == reverse
     }
 }
