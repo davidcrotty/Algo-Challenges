3e23813ad3151f511e60203476467f0f80b98976
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-05 10:15:50 +0000

    failing twosum test

---
+++ app/src/main/java/net/davidcrotty/algochallenges/TwoSum2.kt
@@ -0,0 +1,8 @@
+package net.davidcrotty.algochallenges
+
+class TwoSum2 {
+
+    fun twoSum2(input: IntArray, target: Int): IntArray {
+        return intArrayOf()
+    }
+}
---
+++ app/src/test/java/net/davidcrotty/algochallenges/TwoSum2Test.kt
@@ -0,0 +1,19 @@
+package net.davidcrotty.algochallenges
+
+import org.junit.Assert.assertArrayEquals
+import org.junit.Test
+
+class TwoSum2Test {
+
+    @Test
+    fun twoSum2() {
+        val input = intArrayOf(2,7,11,15)
+        val target = 9
+
+        val sut = TwoSum2()
+        val actual = sut.twoSum2(input, target)
+
+        val expected = intArrayOf(1, 2)
+        assertArrayEquals(expected, actual)
+    }
+}
