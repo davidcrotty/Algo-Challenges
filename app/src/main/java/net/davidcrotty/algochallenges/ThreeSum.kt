558638c018718a94c28ca6bf26f678d7f9d18c8e
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-05 14:56:35 +0000

    passing test

--- app/src/main/java/net/davidcrotty/algochallenges/ThreeSum.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/ThreeSum.kt
@@ -4,36 +4,30 @@ class ThreeSum {
 
     fun threeSum(nums: IntArray): Array<IntArray> {
 
-        var startP = 1
-        var middleP = nums.size / 2
+        val sortedNums = nums.sorted()
+
+        var startP = 0
         var endP = nums.size - 1
 
         val results = mutableListOf<IntArray>()
 
-        while(true) {
-            val start = nums[startP]
-            val middle = nums[middleP]
-            val end = nums[endP]
+        for(i in sortedNums.indices) {
+            val start = sortedNums[startP]
+            val middle = sortedNums[i + 1]
+            val end = sortedNums[endP]
 
             val sum = start + middle + end
             if (sum == 0) {
                 results.add(intArrayOf(start, middle, end))
-
-                if (results.size >= nums.size / 3) {
+                if (results.size == nums.size / 3) {
                     break
                 }
 
-                endP--
                 startP++
-
-                if (middleP > startP && middleP < endP) {
-                    middleP++
-                } else {
-                    middleP--
-                }
-
-            } else {
                 endP--
+            } else if (sum > 0) {
+                endP--
+            } else {
                 startP++
             }
         }
