7ddfd853bcc0b3d74c0a8680ef4c2917657ea997
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-06 10:50:16 +0000

    passing scenario

--- app/src/main/java/net/davidcrotty/algochallenges/ContainerWithMostWater.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/ContainerWithMostWater.kt
@@ -3,6 +3,41 @@ package net.davidcrotty.algochallenges
 class ContainerWithMostWater {
 
     fun containerWithMostWater(verticalLines: IntArray): Int {
-        return 0
+
+        var bestArea = 0
+
+        for (line in verticalLines.withIndex()) {
+            val startP = line.index
+            var endP = line.index + 1
+
+            while(endP < verticalLines.size) {
+
+                var startHeight = verticalLines[startP]
+                var endHeight = verticalLines[endP]
+
+                // tallestpointP, compare pointers
+                val smallestpointP = if (startHeight == endHeight) {
+                    startP
+                } else if(startHeight < endHeight) {
+                    startP
+                } else {
+                    endP
+                }
+
+                val area = if (smallestpointP == startP) {
+                    verticalLines[startP] * (endP - 1)
+                } else {
+                    verticalLines[endP] * (endP - startP)
+                }
+
+                if (area > bestArea) {
+                    bestArea = area
+                }
+
+                endP++
+            }
+        }
+
+        return bestArea
     }
 }
