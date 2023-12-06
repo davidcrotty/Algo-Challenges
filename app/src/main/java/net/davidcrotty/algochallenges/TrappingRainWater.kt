29ed688d82d499ce83cc067e19e4b9d90febf9df
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-06 12:17:42 +0000

    match peaks

--- app/src/main/java/net/davidcrotty/algochallenges/TrappingRainWater.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/TrappingRainWater.kt
@@ -10,25 +10,31 @@ class TrappingRainWater {
 
         var peak1P = 0
         var peak2P = 1
+        var match = false
 
         for(i in height.indices) {
             if (peak1P > height.size - 1) break
+            match = false
 
             var peak1 = height[peak1P]
 
             while (peak2P < height.size) {
                 val peak2 = height[peak2P]
 
-                if (peak1 >= 1 && peak2 > peak1 && peak2P - peak1P > 1) {
+                if (peak1 >= 1 && peak2 >= peak1 && peak2P - peak1P >= 0) {
                     // match, scan for area
                     print("| peak, first: $peak1, second: $peak2 ")
                     // then shift along
                     peak1P = peak2P
-                    peak1 = height[peak1P]
+                    peak2P++
+                    match = true
+                    break
+                } else {
+                    peak2P++
                 }
-
-                peak2P++
             }
+
+            if (match) continue
 
             peak1P++
             peak2P = peak1P + 1
