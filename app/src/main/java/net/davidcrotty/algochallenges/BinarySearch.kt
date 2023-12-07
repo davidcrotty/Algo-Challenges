65100d92f7d07bff278300069cef1f43ea7ac524
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-07 10:56:16 +0000

    binary search

--- app/src/main/java/net/davidcrotty/algochallenges/BinarySearch.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/BinarySearch.kt
@@ -2,8 +2,27 @@ package net.davidcrotty.algochallenges
 
 class BinarySearch {
 
-    fun binarySearch(nums: IntArray): Int {
-        return -1
+    fun binarySearch(nums: IntArray, target: Int): Int {
+
+        val result = search(target, nums, 0, nums.size - 1)
+
+        return result
+    }
+
+    private fun search(target: Int, nums: IntArray, leftP: Int, rightP: Int): Int {
+        var rightPLocal = rightP
+        var leftPLocal = leftP
+
+        val mid = (leftP + rightP) / 2
+        if (nums[mid] == target) {
+            return mid
+        } else if (mid > target) {
+            rightPLocal = mid - 1
+        } else {
+            leftPLocal = mid + 1
+        }
+
+        return search(target, nums, leftPLocal, rightPLocal)
     }
 
 }
--- app/src/test/java/net/davidcrotty/algochallenges/BinarySearchTest.kt
+++ app/src/test/java/net/davidcrotty/algochallenges/BinarySearchTest.kt
@@ -7,9 +7,10 @@ class BinarySearchTest {
     @Test
     fun binarySearch() {
         val input = intArrayOf(-1,0,3,5,9,12)
+        val target = 9
         val sut = BinarySearch()
 
-        val actual = sut.binarySearch(input)
+        val actual = sut.binarySearch(input, target)
 
         val expected = 4
         assertEquals(expected, actual)
