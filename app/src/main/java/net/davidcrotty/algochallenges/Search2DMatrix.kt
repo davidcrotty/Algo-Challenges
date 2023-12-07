12dc574e4c410911722110fbdc8f3f62897da782
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-07 14:13:34 +0000

    binary search column

--- app/src/main/java/net/davidcrotty/algochallenges/Search2DMatrix.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/Search2DMatrix.kt
@@ -1,8 +1,37 @@
 package net.davidcrotty.algochallenges
 
+import android.graphics.Point
+
 class Search2DMatrix {
 
     fun search2DMatrix(matrix: Array<IntArray>, target: Int): Boolean {
+
+
+
+        return search(matrix, target, 0, (matrix.size - 1)  * (matrix.size - 1))
+    }
+
+    private fun search(matrix: Array<IntArray>, target: Int, start: Int, end: Int): Boolean {
+
+        if (start >= end) return false
+
+        val col = (end - start) / 2
+        val i = col % 2
+        val j = col / 2
+
+        val mid = matrix[i][j]
+
+        if (mid == target) {
+            return true
+        } else if (mid > target) {
+            // left
+            var endLocal = end - 1
+            search(matrix, target, start, endLocal)
+        } else {
+            var startLocal = start + 1
+            search(matrix, target, startLocal, end)
+        }
+
         return false
     }
 }
--- app/src/test/java/net/davidcrotty/algochallenges/Search2DMatrixTest.kt
+++ app/src/test/java/net/davidcrotty/algochallenges/Search2DMatrixTest.kt
@@ -20,4 +20,20 @@ class Search2DMatrixTest {
         val expected = true
         assertEquals(expected, actual)
     }
+
+    @Test
+    fun search2DMatrix2() {
+        val matrix = arrayOf(
+            intArrayOf(1,3,5,7),
+            intArrayOf(10,11,16,20),
+            intArrayOf(23,30,34,60)
+        )
+        val target = 13
+
+        val sut = Search2DMatrix()
+        val actual = sut.search2DMatrix(matrix, target)
+
+        val expected = false
+        assertEquals(expected, actual)
+    }
 }
