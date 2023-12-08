edac16442d9932f88d5a3761a2bb350163c424b2
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-08 13:59:50 +0000

    passing bananas test

--- app/src/main/java/net/davidcrotty/algochallenges/KokoEatsBananas.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/KokoEatsBananas.kt
@@ -2,7 +2,56 @@ package net.davidcrotty.algochallenges
 
 class KokoEatsBananas {
 
-    fun minEatingSpeed(pile: IntArray): Int {
-        return 0
+    fun minEatingSpeed(pile: IntArray, h: Int): Int {
+
+        val sortedPile = pile.sorted().toMutableList()
+
+        val largest = sortedPile.last()
+        val possibleKValues = mutableListOf<Int>()
+
+        for (i in 0..largest) {
+            possibleKValues.add(i)
+        }
+
+        var start = 0
+        var end = possibleKValues.size - 1
+
+        var currentK = 0
+
+        while(start <= end) {
+
+            val mid = (start + end) / 2
+
+            val hours = checkBananaHours(pile, possibleKValues[mid])
+            // grab mid, brute force array, is it too fast or too slow? (== h?)
+
+            if (hours <= h) {
+                currentK = hours
+            }
+
+            // force k value
+            if (hours < h) {
+                // left
+                end = mid - 1
+            } else {
+                // right
+                start = mid + 1
+            }
+        }
+
+
+        return currentK
+    }
+
+    private fun checkBananaHours(pile: IntArray, k: Int): Int {
+
+        var totalHours = 0
+
+        for (i in pile.indices) {
+            var hoursInPile = pile[i] / k
+            totalHours = totalHours + hoursInPile
+        }
+
+        return totalHours
     }
 }
--- app/src/test/java/net/davidcrotty/algochallenges/KokoEatsBananasTest.kt
+++ app/src/test/java/net/davidcrotty/algochallenges/KokoEatsBananasTest.kt
@@ -8,9 +8,10 @@ class KokoEatsBananasTest {
     @Test
     fun bananaPile() {
         val bananaPile = intArrayOf(3,6,7,11)
+        val hours = 8
 
         val sut = KokoEatsBananas()
-        val actual = sut.minEatingSpeed(bananaPile)
+        val actual = sut.minEatingSpeed(bananaPile, hours)
 
         val expected = 4
         assertEquals(expected, actual)
