b56afaff6c004028067d7e896fd8c89d5be571cf
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-11 11:44:08 +0000

    first attempt at find min

--- app/src/main/java/net/davidcrotty/algochallenges/MinimumRotatedSortedArray.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/MinimumRotatedSortedArray.kt
@@ -3,6 +3,27 @@ package net.davidcrotty.algochallenges
 class MinimumRotatedSortedArray {
 
     fun findMin(nums: IntArray): Int {
+
+        var left = 0
+        var right = nums.size
+
+        while (left < right) {
+            var mid = (left + right) / 2
+
+            val midValue = nums[mid]
+
+            if (midValue > nums[mid - 1] && midValue > nums[mid + 1]) {
+                return Math.min(nums[mid - 1], nums[mid + 1])
+            } else if (midValue < nums[mid - 1]) {
+                mid--
+                left = mid
+            } else {
+                mid++
+                right = mid
+            }
+
+        }
+
         return -1
     }
 }
