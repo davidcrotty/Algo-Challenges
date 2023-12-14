178783a748bf6cae0b268ec1d08b548f060b4923
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-14 18:07:08 +0000

    passing test

--- app/src/main/java/net/davidcrotty/algochallenges/MedianSortedArrays.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/MedianSortedArrays.kt
@@ -4,40 +4,51 @@ class MedianSortedArrays {
 
     fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
 
-        var start1P = 0
-        var end1P = nums1.size - 1
+        // find partition
+        val totalSize = nums1.size + nums2.size
 
-        var start2P = 0
-        var end2P = nums2.size - 1
-
-        val is1Even = (nums1.size % 2) == 0
-        val is2Even = (nums2.size % 2) == 0
-
-        val mid1P = (start1P + end1P) / 2
-        val mid2P = (start2P + end2P) / 2
-
-        if(!is1Even && !is2Even) {
-            return (nums1[mid1P] + nums2[mid2P]) / 2.0
-        }
-
-        if(is1Even && is2Even) {
-            // TODO bounds check
-            val median1 =  (nums1[mid1P] + nums1[mid1P + 1]) / 2.0
-            val median2 =  (nums2[mid2P] + nums2[mid2P + 1]) / 2.0
-            return (median1 + median2) / 2.0
-        }
-
-        if (is1Even && !is2Even) {
-            val median1 = (nums1[mid1P] + nums1[mid1P + 1]) / 2.0
-            val median2 = nums2[mid2P]
-            return (median1 + median2) / 2.0
+        val smallest = Math.min(nums1.size, nums2.size)
+        val smallestArray = if (smallest == nums1.size) {
+            nums1
         } else {
-            val median1 = nums1[mid1P]
-            val median2 =  (nums2[mid2P] + nums2[mid2P + 1]) / 2.0
-            return (median1 + median2) / 2.0
+            nums2
         }
 
+        val largest = Math.max(nums1.size, nums2.size)
+        val largestArray = if (smallest == nums1.size) {
+            nums2
+        } else {
+            nums1
+        }
 
+        var smallLeftP = 0
+        var smallRightP = smallestArray.size - 1
+
+        // loop
+        while(smallLeftP < smallRightP) {
+            var smallestLeftMid = (smallLeftP + smallRightP) / 2
+
+            val largestLeftMid = (totalSize / 2) - smallestLeftMid
+
+            // validate partition
+            if (smallestArray[smallestLeftMid] <= largestArray[largestLeftMid + 1] == false ||
+                largestArray[largestLeftMid] <= smallestArray[smallestLeftMid + 1] == false) {
+                smallestLeftMid++
+                smallLeftP = smallestLeftMid
+            } else {
+                // if even total
+                if (totalSize % 2 == 0) {
+                    val maxLeft = Math.max(smallestArray[smallestLeftMid] , largestArray[smallestLeftMid])
+                    val maxRight = Math.min(smallestArray[smallestLeftMid + 1], largestArray[smallestLeftMid + 1])
+                    return (maxLeft + maxRight) / 2.0
+                } else {
+                    return Math.min(smallestArray[smallestLeftMid].toDouble(), largestArray[largestLeftMid].toDouble())
+                }
+                // take min/max from both right/left
+                // else min between mid 2
+            }
+
+        }
 
         return 0.0
     }
