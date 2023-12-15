1886782fa3b7b1e3a167c3b89a00bb2067b71f4a
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-15 17:26:17 +0000

    closest to solution

--- app/src/main/java/net/davidcrotty/algochallenges/MedianSortedArrays.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/MedianSortedArrays.kt
@@ -1,42 +1,56 @@
 package net.davidcrotty.algochallenges
 
+import java.lang.Double.NEGATIVE_INFINITY
+import java.lang.Double.POSITIVE_INFINITY
+
 class MedianSortedArrays {
 
     fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
 
-        val smallest = if (nums1.size < nums2.size) {
+        var smallest = if (nums1.size < nums2.size) {
             nums1
         } else {
             nums2
         }
 
-        val largest = if (nums1.size > nums2.size) {
+        var largest = if (nums1.size > nums2.size) {
             nums1
         } else {
             nums2
+        }
+
+        if (nums1.size == nums2.size) {
+            smallest = nums1
+            largest = nums2
         }
 
         var leftP = 0
         var rightP = smallest.size - 1
-        val half = ((smallest.size + largest.size) / 2)
+        val half = Math.floorDiv(smallest.size + largest.size, 2)
 
         while (true) {
-            var smallMidP = (leftP + rightP) / 2 // smallest
+            var smallMidP = Math.floorDiv(leftP + rightP, 2) // smallest
             var largeMidP =  half - smallMidP - 2 // largest
 
-            if (smallest[smallMidP] <= largest[largeMidP + 1] &&
-                largest[largeMidP] <= smallest[smallMidP + 1]) {
+            // both array extremes need accounting for
+            val leftMiddle = if (smallMidP < 0) NEGATIVE_INFINITY else smallest[smallMidP].toDouble()
+            val leftNext = if (smallMidP + 1 > smallest.size) POSITIVE_INFINITY else smallest[smallMidP + 1].toDouble()
+            val rightNext = if (largeMidP + 1 > largest.size) POSITIVE_INFINITY else largest[largeMidP + 1].toDouble()
+            val rightMiddle = if (largeMidP < 0) NEGATIVE_INFINITY else largest[largeMidP].toDouble()
+
+            if (leftMiddle <= rightNext &&
+                rightMiddle <= leftNext) {
 
                 if (smallest.size % 2 == 0) {
                     // even case
-                    val max = Math.max(smallest[smallMidP], largest[largeMidP])
-                    val min = Math.min(largest[largeMidP + 1], smallest[smallMidP + 1])
+                    val max = Math.max(leftMiddle, rightMiddle)
+                    val min = Math.min(leftNext, rightNext)
                     return (max + min) / 2.0
                 } else {
                     // odd case
-                    return Math.min(largest[largeMidP + 1].toDouble(), smallest[smallMidP + 1].toDouble())
+                    return Math.min(rightNext, leftNext)
                 }
-            } else if (smallest[smallMidP] > largest[largeMidP + 1] == false) {
+            } else if (leftMiddle < rightNext) {
                 // smallest is wrong
                 rightP = smallMidP - 1
             } else {
