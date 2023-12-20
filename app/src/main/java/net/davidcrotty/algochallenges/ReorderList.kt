5a23c5e219e2624b47ea5d5d894620129dc86b22
davidcrotty <david.crotty@playsportsnetwork.com>
2023-12-20 13:05:06 +0000

    part 1 sub problem

--- app/src/main/java/net/davidcrotty/algochallenges/ReorderList.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/ReorderList.kt
@@ -4,7 +4,23 @@ import net.davidcrotty.algochallenges.data.ListNode
 
 class ReorderList {
 
-    fun reorderList(head: ListNode): ListNode {
+    fun reorderList(head: ListNode?): ListNode {
+        // n = end of list
+        // 1 - n - 2 - n(-1) - 3 - n(-2)
+
+        var current = head
+
+        // get size
+        var listSize = 0
+        while (current != null) {
+            current = current.next
+            listSize++
+        }
+
+        // loop and alternate each one
+
+        // add in inner loop for n - 1 using size
+
         return ListNode(0)
     }
 }
