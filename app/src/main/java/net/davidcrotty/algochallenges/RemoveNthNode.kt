e736292c606b7606bc5f0984fb075a80a2594b60
davidcrotty <david.crotty@playsportsnetwork.com>
2024-01-02 16:35:02 +0000

    passing test

--- app/src/main/java/net/davidcrotty/algochallenges/RemoveNthNode.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/RemoveNthNode.kt
@@ -6,8 +6,43 @@ class RemoveNthNode {
 
     fun removeNthNode(head: ListNode, n: Int): ListNode {
 
+        var current: ListNode? = head
+        var index = 0
 
+        // get size
+        while (current != null) {
+            current = current.next
+            index++
+        }
 
-        return ListNode(0)
+        // calculate target
+        var target = index - n
+
+        // store previous target, iterate to next and re-attach
+        var mutableHead = head
+        var iterableHead: ListNode? = mutableHead
+        var counter = 0
+
+        var firstSegment: ListNode? = null
+
+        while (iterableHead != null) {
+
+            if (counter + 1 == target) {
+                // store head
+                firstSegment = iterableHead
+            }
+
+            if (counter == target) {
+                // re-attach to first segment
+                val lastSegment = iterableHead.next
+                firstSegment?.next = lastSegment
+            }
+
+            // move along
+            iterableHead = iterableHead.next
+            counter++
+        }
+
+        return mutableHead
     }
 }
