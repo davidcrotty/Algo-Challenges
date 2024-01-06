76a8cb08b5d91aaad8dad06e2c54d3bafb4ac524
davidcrotty <david.crotty@playsportsnetwork.com>
2024-01-06 10:45:28 +0000

    invert small tree

--- app/src/main/java/net/davidcrotty/algochallenges/InvertBinaryTree.kt
+++ app/src/main/java/net/davidcrotty/algochallenges/InvertBinaryTree.kt
@@ -5,6 +5,13 @@ import net.davidcrotty.algochallenges.data.TreeNode
 class InvertBinaryTree {
 
     fun invertBinaryTree(input: TreeNode): TreeNode {
+        return invert(input)
+    }
+
+    private fun invert(input: TreeNode): TreeNode {
+        val tmp = input.left
+        input.left = input.right
+        input.right = tmp
         return input
     }
 }
